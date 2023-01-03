fd94ce5ed6ba0c9fee5cba8efb43a7e16e225db4
onlinedev0808 <onlinedev0808@gmail.com>
2023-01-03 22:12:01 +0200

    CSS refactoring

--- frontend/src/components/recipes/SmallRecipeItem.js
+++ frontend/src/components/recipes/SmallRecipeItem.js
@@ -1,32 +1,32 @@
 import React from "react";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import classes from "./SmallRecipeItem.module.css";
-import Button from '../layout/Button'
+import Button from "../layout/Button";
 import Line from "../layout/Line";
 
 const SmallRecipeItem = ({ recipe }) => {
   const recipeExcerpt = recipe.preparation.substring(0, 150) + "...";
-  const user = recipe.user
+  const user = recipe.user;
 
   return (
     <React.Fragment>
       <div className={classes.recipesList}>
-        <img className={classes.smallPic}
+        <img
+          className={classes.smallPic}
           src={require(`../../../public/uploads/${recipe.photos}`)}
           alt="chicken-breast"
         ></img>
         <div>
-          <h4 className={classes.recipeTitle}>{recipe.title}</h4>
+          <h2>{recipe.title}</h2>
           <Line />
           {/* <Link to={`/profile/${user}`}>Виж</Link> */}
           <p className={classes.recipeExerpt}>{recipeExcerpt}</p>
           <div className={classes.btnDiv}>
-          <Link className={classes.btn} to={`/recepti/${recipe._id}`}>ВИЖ ПОВЕЧЕ</Link>
+            <Link className="btn__primary" to={`/recepti/${recipe._id}`}>
+              ВИЖ ПОВЕЧЕ
+            </Link>
           </div>
-          
-          
         </div>
-        
       </div>
     </React.Fragment>
   );
